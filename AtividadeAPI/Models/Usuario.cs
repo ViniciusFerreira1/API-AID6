fc15bf47f5088dfb366654280fc12ea627db48b5
Vinicius Ferreira <viniciusferreira365@gmail.com>
2020-11-30 20:51:38 -0300

    Criacao do CRUD basico post OK

---
+++ AtividadeAPI/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeAPI.Models
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
